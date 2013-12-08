c74203aea40d8c24795c7459cfcb898da6d15800
av11d <av11du@gmail.com>
2013-12-08 04:14:33 -0500

    Added mongoose

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,15 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  var mongoose = require('mongoose');
+
+  mongoose.connect('mongodb://crewfiresvc:Axvalanch1@ds053838.mongolab.com:53838/crewfire');
+
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function callback () {
+        res.render('index', { title: 'Express' });
+    });
+
+
 };
